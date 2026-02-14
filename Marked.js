c252466d27c874c6e2cda8b6d764bee1176825f1
biocorp80 <biocorp80@gmail.com>
2026-02-14 16:14:28 +0700

    Create Marked.js

---
+++ Marked.js
@@ -0,0 +1,4 @@
+<script src="https://cdn.tailwindcss.com"></script>
+<script src="https://unpkg.com/html-docx-js/dist/html-docx.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.5/FileSaver.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
